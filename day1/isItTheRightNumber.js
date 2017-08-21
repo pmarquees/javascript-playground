5550dec0b09fb000dc64615857b10112a1794e66
Pedro Marques <pedro.marques@booking.com>
2017-08-21 17:15:21 +0200

    Simple function to get a number from the user and checking if that number is the one I set, I also added something to check if the user has actually filled the prompt and if that is a number

---
+++ day1/isItTheRightNumber.js
@@ -0,0 +1,14 @@
+var userInput = prompt("What's the number yo");
+var userResponse = parseInt(userInput);
+var rightNumber = 6;
+
+userResponse
+if (userResponse === rightNumber) {
+	console.log("Yep");
+} else if (userInput == null || userResponse != isNaN ){
+	console.log("Type a number");
+} else {
+	console.log("Nope");
+}
+
+
